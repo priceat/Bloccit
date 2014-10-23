bed8b2d7aea54a9842abceda37d7d66d58cfeea2
Tate Price <priceat@gmail.com>
2014-10-23 12:13:28 -0500

    Added Pundit and Defined User Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def admin?
+    role == 'admin'
+  end
+
+  def moderator?
+    role == 'moderator'
+  end
   
 
 end
