8aff2d59b8dfc519aa33875171b1144360da9d9c
Tate Price <priceat@gmail.com>
2014-10-23 10:08:11 -0500

    Changed Gemfile and added User associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 require 'faker'
 
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+#Topic.destroy_all
+
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 50.times do 
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
@@ -15,6 +32,12 @@ posts = Post.all
     )
 end
 
+User.first.update_attributes!(
+  email: 'priceat@gmail.com',
+  password: 'helloworld',
+  )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
