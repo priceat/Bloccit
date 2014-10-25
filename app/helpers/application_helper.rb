285096880e0a836995ef71b581dd8d472c5715bf
Tate Price <priceat@gmail.com>
2014-10-25 17:25:50 -0500

    Error messages and partial rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def my_name
     "Tate Price aka Voltron"
   end
+
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form_group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
