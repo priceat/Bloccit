982fcb83eccc8f2bc200c4e857f1bab3bc72690e
Tate Price <priceat@gmail.com>
2014-10-28 13:59:35 -0500

    Comments, etc

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:notice] = "Comment was created."
+      redirect_to [@topic,@post]
+    else
+      flash[:error] = "Comment failed to save."
+      redirect_to [@topic,@post]
+    end
+  end
+
+
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Bloccit::Application.routes.draw do
    
   
+  get "comments/create"
   devise_for :users
     resources :users, only: [:update]
   resources :topics do
     resources :posts, except: [:index]
+  end
+  resources :posts, only: [:index] do
+    resources :comments, only: [:create]
   end
 
    
