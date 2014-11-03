8227b5edd7b1ddeea01f64a70c59a5319012ee2a
Tate Price <priceat@gmail.com>
2014-11-03 09:38:55 -0600

    Updated

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,8 +2,8 @@ class Topic < ActiveRecord::Base
 
 has_many :posts, dependent: :destroy
 
-  scope :publicly_visible { where(public:true) }
-  scope :privately_visible { where(public:false) }
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :public, -> { where(public:true) }
+  scope :private, -> { where(public:false) }
+  scope :visible_to, -> (user) { user ? all : public }
 
 end
